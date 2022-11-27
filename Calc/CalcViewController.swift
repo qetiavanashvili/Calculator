3fef3afa5e2be5eb1072174c014a5a2c9265314b
Qeti Avanashvili <qetiavanashvili@gmail.com>
2022-11-27 17:08:25 +0400

    Format code

--- Calc/CalcViewController.swift
+++ Calc/CalcViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 
 class CalcViewController: UIViewController {
     
-    // MARK: IBOotlets
+    // MARK: IBOutlets
 
     @IBOutlet weak var lcdDisplay: UIView!
     @IBOutlet weak var displayLabel: UILabel!
-    
     
     @IBOutlet weak var pinpadButton0: UIButton!
     @IBOutlet weak var pinpadButton1: UIButton!
@@ -27,11 +26,9 @@ class CalcViewController: UIViewController {
     @IBOutlet weak var pinpadButton8: UIButton!
     @IBOutlet weak var pinpadButton9: UIButton!
     
-    
     @IBOutlet weak var clearButton: UIButton!
     @IBOutlet weak var negateButton: UIButton!
     @IBOutlet weak var percentageButton: UIButton!
-    
     
     @IBOutlet weak var equalButton: UIButton!
     @IBOutlet weak var divideButton: UIButton!
@@ -41,11 +38,9 @@ class CalcViewController: UIViewController {
     
     @IBOutlet weak var decimalButton: UIButton!
     
-    
     // MARK: Color Themes
     var currentTheme: CalculatorTheme {
         return CalculatorTheme(extraFunctionColor: "#a6a6a6", extraFunctionTitleColor: "#FFFFFF", backgroundColor: "#000000", displayColor: "#FFFFFF", operationColor: "#ff9f0a", operationTitleColor: "#FFFFFF", pinpadColor: "#333333", pinpadTitleColor: "#FFFFFF")
-        
     }
     
     // MARK: - Calculator Engine
@@ -59,7 +54,6 @@ class CalcViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    
     // MARK: Decorate
     
     private func decorateView() {
@@ -69,12 +63,9 @@ class CalcViewController: UIViewController {
         displayLabel.textColor = UIColor(hex: currentTheme.displayColor)
         displayLabel.backgroundColor = UIColor(hex: currentTheme.backgroundColor)
         decorateButtons()
-        
     }
     
     private func decorateButtons() {
-        
-        
         decoratePinPadButton(pinpadButton0, true)
         decoratePinPadButton(pinpadButton1)
         decoratePinPadButton(pinpadButton2)
@@ -90,16 +81,12 @@ class CalcViewController: UIViewController {
         decorateExtraFunctionButton(negateButton)
         decorateExtraFunctionButton(percentageButton)
         
-        
         decorateOperationButton(divideButton)
         decorateOperationButton(multiplyButton)
         decorateOperationButton(addButton)
         decorateOperationButton(minusButton)
         decorateOperationButton(equalButton)
-        
         decoratePinPadButton(decimalButton)
-        
-        
     }
     
     
@@ -107,12 +94,10 @@ class CalcViewController: UIViewController {
         let image = usingSlicedImage ? UIImage(named: "CircleSliced") : UIImage(named: "Circle")
         button.setBackgroundImage(image, for: .normal)
         button.backgroundColor = .clear
-        
     }
     
     private func decorateExtraFunctionButton(_ button:UIButton) {
         decorateButton(button)
-        
         
         button.tintColor = UIColor(hex: currentTheme.extraFunctionColor)
         button.setTitleColor(UIColor(hex: currentTheme.extraFunctionTitleColor), for: .normal)
@@ -126,8 +111,8 @@ class CalcViewController: UIViewController {
         button.setTitleColor(UIColor(hex: currentTheme.operationTitleColor), for: .normal)
         
         button.titleLabel?.font = UIFont.systemFont(ofSize: 50)
-        
     }
+    
     private func decoratePinPadButton(_ button: UIButton, _ usingSlicedImage: Bool = false) {
         decorateButton(button, usingSlicedImage)
         
@@ -142,7 +127,6 @@ class CalcViewController: UIViewController {
     @IBAction private func clearPressed() {
         calculatorEngine.clearPressed()
         refreshLCDDisplay()
-        
     }
     
     @IBAction private func negatePressed() {
@@ -200,9 +184,4 @@ class CalcViewController: UIViewController {
     private func refreshLCDDisplay () {
         displayLabel.text = calculatorEngine.lcdDisplayText
     }
-    
-    
-    
 }
-
-
