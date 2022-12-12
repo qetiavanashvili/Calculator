a6fa658ba57f5c907844ee71394f4c00db5634cc
Qeti Avanashvili <qetiavanashvili@gmail.com>
2022-12-12 19:23:22 +0400

    Show the copy-paste on the display

--- Calc/Presentation Layer/UIKIT Subclasses/LCDDisplay.swift
+++ Calc/Presentation Layer/UIKIT Subclasses/LCDDisplay.swift
@@ -68,12 +68,35 @@ class LCDDisplay: UIView {
         }
     }
     
+    // MARK: - UIMenuController
+    
     private func showMenu(from gestuRecognizer: UILongPressGestureRecognizer) {
+        
+        becomeFirstResponder()
+        
         let menu = UIMenuController.shared
         guard menu.isMenuVisible == false else { return }
-            
-        // we want to show menu here
-        print("show menu")
         
+        let locationOfTouch = gestuRecognizer.location(in: self)
+        var rect = bounds
+        rect.origin = locationOfTouch
+        rect.origin.y = rect.origin.y - 20
+        rect.size = CGSize(width: 1, height: 44)
+        menu.showMenu(from: self, rect: rect)
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        return action == #selector(UIResponderStandardEditActions.copy(_:)) || action == #selector(UIResponderStandardEditActions.paste(_:))
+    }
+    @objc override func copy(_ sender: Any?) {
+        print("Copy was pressed")
+    }
+    
+    @objc override func paste(_ sender: Any?) {
+        print("Paste was pressed")
     }
 }
