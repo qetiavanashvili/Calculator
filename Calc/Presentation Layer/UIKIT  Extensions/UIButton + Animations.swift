552b4251c0beb89a4dacec626125d9acdf12506c
Qeti Avanashvili <qetiavanashvili@gmail.com>
2022-12-16 15:05:42 +0400

    Make to work button animations

--- Calc/Presentation Layer/UIKIT  Extensions/UIButton + Animations.swift
+++ Calc/Presentation Layer/UIKIT  Extensions/UIButton + Animations.swift
@@ -11,7 +11,46 @@ import UIKit
 extension UIButton {
     
     func bounce() {
-        
+        moveUp()
     }
     
-}
+    
+    func growLarger() {
+        UIView.animate(withDuration: 1, delay: 0, options: .curveEaseIn) {
+            self.transform = CGAffineTransform(scaleX: 4, y: 4)
+        } completion: { _ in
+            
+        }
+    }
+        
+        func makeSmaller() {
+            UIView.animate(withDuration: 1, delay: 0, options: .curveEaseIn) {
+                self.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            } completion: { _ in
+                
+            }
+                
+    }
+    
+    func moveUp() {
+        UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut) {
+            self.transform = CGAffineTransform(translationX: 0, y: -50)
+        } completion: { _ in
+            UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseInOut) {
+                self.transform = CGAffineTransform(translationX: 0, y: 5)
+            } completion: { _ in
+                UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseInOut) {
+                        self.transform = CGAffineTransform(translationX: 0, y: -2)
+                    } completion: { _ in
+                        UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseInOut) {
+                            self.transform = CGAffineTransform(translationX: 0, y: 0)
+                        } completion: { _ in
+                    
+                        }
+                    }
+                }
+            }
+        
+        }
+    
+    }
