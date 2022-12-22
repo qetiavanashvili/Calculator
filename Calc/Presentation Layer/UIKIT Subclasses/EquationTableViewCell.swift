27a1ef84d819fb502958c9c6eec16ce3f0b9b875
Qeti Avanashvili <qetiavanashvili@gmail.com>
2022-12-23 01:08:45 +0400

    Add tick animation

--- Calc/Presentation Layer/UIKIT Subclasses/EquationTableViewCell.swift
+++ Calc/Presentation Layer/UIKIT Subclasses/EquationTableViewCell.swift
@@ -12,16 +12,31 @@ class EquationTableViewCell: UITableViewCell {
     @IBOutlet var lhslabel: UILabel!
     @IBOutlet var rhslabel: UILabel!
     @IBOutlet var resultlabel: UILabel!
-
+    @IBOutlet var tick: UIImageView!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        tick.alpha = 0
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
+    func displayTick() {
+        UIView.animate(withDuration: 0.25, delay: 0, options: []) { [weak self] in
+            self?.tick.alpha = 1
+            self?.tick.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        } completion: { _ in
+            UIView.animate(withDuration: 0.1, delay: 0, options: []) { [weak self] in
+                self?.tick.transform = CGAffineTransform.identity
+            } completion: { _ in
+                
+            }
+        }
+        
+    }
 }
--- Calc/Presentation Layer/UIViewControllers/LogViewController.swift
+++ Calc/Presentation Layer/UIViewControllers/LogViewController.swift
@@ -12,10 +12,10 @@ class LogViewController: UITableViewController {
     // MARK: - Datasource
     
     var datasource: [MathEquation] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        decorateView()
         setupNavigationBar()
     }
     
@@ -25,46 +25,87 @@ class LogViewController: UITableViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(self.doneButtonPressed))
         
     }
-                                                            
+    
     @objc private func doneButtonPressed() {
         dismiss(animated: true, completion: nil)
-            
-        }
-
+        
+    }
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return datasource.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "EquationTableViewCell", for: indexPath) as? EquationTableViewCell else {
             return UITableViewCell()
         }
-
+        
         
         let equation = datasource[indexPath.row]
         cell.lhslabel.text = equation.lhs.formatted()
         cell.rhslabel.text = equation.generateStringRepresentationOfOperation() + " " + (equation.rhs?.formatted() ?? "")
         cell.resultlabel.text = "= " + (equation.result?.formatted() ?? "")
-        
+        cell.selectedBackgroundView = UIView()
+        decorateCell(cell)
         
         return cell
     }
     
+    
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+         
+        guard let cell = tableView.cellForRow(at: indexPath) as? EquationTableViewCell else {
+            return
+        }
         
         let equation = datasource[indexPath.row]
         let userInfo: [AnyHashable: Any] = ["PasteKey" : equation ]
         NotificationCenter.default.post(name: NSNotification.Name("iOSBFRee.com.Calc.LogViewController.pasteMathEquation"), object: nil, userInfo: userInfo)
         
-        dismiss(animated: true, completion: nil)
+        tableView.isUserInteractionEnabled = false
+        cell.displayTick()
+        dismissAfterDelay()
+    }
+    
+    private func dismissAfterDelay() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
+            self?.dismiss(animated: true, completion: nil)
+        }
     }
     
     
+    // MARK: - Decorate
+    
+    private func decorateCell(_ cell: EquationTableViewCell) {
+        let theme = ThemeManager.shared.currentTheme
+        cell.backgroundColor = UIColor(hex: theme.backgroundColor)
+        cell.selectedBackgroundView?.backgroundColor = UIColor(hex: theme.operationColor)
+        cell.lhslabel.textColor = UIColor(hex: theme.displayColor)
+        cell.rhslabel.textColor = UIColor(hex: theme.displayColor)
+        cell.resultlabel.textColor = UIColor(hex: theme.displayColor)
+        cell.lhslabel.highlightedTextColor = UIColor(hex: theme.backgroundColor)
+        cell.rhslabel.highlightedTextColor  = UIColor(hex: theme.backgroundColor)
+        cell.resultlabel.highlightedTextColor  = UIColor(hex: theme.backgroundColor)
+        cell.tick.tintColor = UIColor(hex: theme.operationTitleColor)
+    }
+    
+    private func decorateView() {
+        let theme = ThemeManager.shared.currentTheme
+        tableView.backgroundColor = UIColor(hex: theme.backgroundColor)
+        tableView.tintColor = UIColor(hex: theme.displayColor)
+        
+        tableView.separatorColor = UIColor(hex: theme.displayColor)
+        switch theme.statusBarStyle {
+        case .light: tableView.indicatorStyle = .white
+        case .dark: tableView.indicatorStyle = .black
+        }
+    }
 }
