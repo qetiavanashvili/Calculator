e4b95f540b94732a9aa96f5f013c36969304d201
Qeti Avanashvili <qetiavanashvili@gmail.com>
2022-12-27 23:32:26 +0400

    Test continuous addition

--- Unit Tests/CalculatorEngine/CalculatorEngineAdditionTests.swift
+++ Unit Tests/CalculatorEngine/CalculatorEngineAdditionTests.swift
@@ -11,18 +11,88 @@ import XCTest
 class CalculatorEngineAdditionTests: XCTestCase {
     
     func testExample() throws {
-        // setup
         var calculatorEngine = CalculatorEngine()
         
-        //input values
         calculatorEngine.pinPadPressed(1)
         calculatorEngine.addPressed()
         calculatorEngine.pinPadPressed(1)
         calculatorEngine.equalsPressed()
         
-        
-        //tests
         XCTAssertTrue(calculatorEngine.lcdDisplayText == "2")
     }
+    
+    
+    // MARK: - Equals Button
+    
+    func testPinPadNumberWithEquals() throws {
+        
+        for lhsPinPadNumber in 0...9 {
+            for rhsPinPadNumber in 0...9 {
+                var calculatorEngine = CalculatorEngine()
+                calculatorEngine.pinPadPressed(lhsPinPadNumber)
+                calculatorEngine.addPressed()
+                calculatorEngine.pinPadPressed(rhsPinPadNumber)
+                calculatorEngine.equalsPressed()
+                
+                XCTAssertTrue(calculatorEngine.lcdDisplayText == "\(lhsPinPadNumber + rhsPinPadNumber)")
+            }
+        }
+        
+    }
+    
+    
+    func testContinuousPinPadNumberWithEquals() throws {
+        var calculatorEngine = CalculatorEngine()
+        
+        for lhsPinPadNumber in 0...9 {
+            for rhsPinPadNumber in 0...9 {
+                
+                calculatorEngine.pinPadPressed(lhsPinPadNumber)
+                calculatorEngine.addPressed()
+                calculatorEngine.pinPadPressed(rhsPinPadNumber)
+                calculatorEngine.equalsPressed()
+                
+                XCTAssertTrue(calculatorEngine.lcdDisplayText == "\(lhsPinPadNumber + rhsPinPadNumber)")
+            }
+        }
+        
+    }
+    
+    // MARK: - Operation Buttons
+    func testPinPadNumberByPassingEquals() throws {
+        
+        for lhsPinPadNumber in 0...9 {
+            for rhsPinPadNumber in 0...9 {
+                var calculatorEngine = CalculatorEngine()
+                calculatorEngine.pinPadPressed(lhsPinPadNumber)
+                calculatorEngine.addPressed()
+                calculatorEngine.pinPadPressed(rhsPinPadNumber)
+                calculatorEngine.addPressed()
+                
+                XCTAssertTrue(calculatorEngine.lcdDisplayText == "\(lhsPinPadNumber + rhsPinPadNumber)")
+            }
+        }
+    }
+    
+    func testContinuousPinPadNumberByPassingEquals() throws {
+        
+        for lhsPinPadNumber in 0...9 {
+                var calculatorEngine = CalculatorEngine()
+                calculatorEngine.pinPadPressed(lhsPinPadNumber)
+                calculatorEngine.addPressed()
+                var result = lhsPinPadNumber
+            
+            for rhsPinPadNumber in 0...9 {
+                calculatorEngine.pinPadPressed(rhsPinPadNumber)
+                calculatorEngine.addPressed()
+                result = result + rhsPinPadNumber
+                
+                XCTAssertTrue(calculatorEngine.lcdDisplayText == "\(result)")
+            }
+        }
+    }
+    
+    
+    
     
 }
